abac91b50780409cb0b72866c2db04c4804f32a7
Jasonjason10 <ofil0001@hz.nl>
2023-01-17 12:25:07 +0100

    Added state

---
+++ src/hz/MouseState.java
@@ -0,0 +1,18 @@
+public interface MouseState {
+
+    void Removebattery();
+
+    void InsertBattery();
+
+
+
+    void on();
+
+    void off();
+
+    void broken();
+
+    void idle();
+
+    void fixed();
+}
