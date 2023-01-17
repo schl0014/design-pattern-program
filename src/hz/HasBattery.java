d6b4c8d2560057355c4fc39fe08638e45b9be4f2
niek726 <schl0014@hz.nl>
2023-01-17 12:38:45 +0100

    hasbattery

---
+++ src/hz/HasBattery.java
@@ -0,0 +1,21 @@
+public class HasBattery implements  MouseState{
+    @Override
+    public void Removebattery() {
+        System.out.println("You have removed the battery");
+    }
+
+    @Override
+    public void InsertBattery() {
+        System.out.println("You have inserted the battery");
+    }
+
+    @Override
+    public void BatteryFull() {
+        System.out.println("battery is full");
+    }
+
+    @Override
+    public void BatteryEmpty() {
+        System.out.println("battery is empty change the battery");
+    }
+        }
