2aba40bd6bfd24ee6616e2e361b3fe69fcda4de3
Jasonjason10 <ofil0001@hz.nl>
2023-01-18 11:34:39 +0100

    Started with state

--- src/hz/Main.java
+++ src/hz/Main.java
@@ -3,5 +3,15 @@ public class Main {
     public static void main(String[] args) {
         MouseFacade facade = new MouseFacade();
         facade.createMouseInstruction();
+
+        Mouse mouse = new Mouse();
+
+        mouse.BatteryFull();
+        mouse.InsertBattery();
+        mouse.BatteryEmpty();
+        mouse.RemoveBattery();
+
+
+
     }
 }
--- src/hz/Mouse.java
+++ src/hz/Mouse.java
@@ -1,18 +1,31 @@
 public class Mouse {
 
+    MouseState HasBattery;
+    MouseState HasNoBattery;
+
+    MouseState state;
+
+    public Mouse(){
+        HasBattery = new HasBattery();
+        HasNoBattery = new HasNoBattery();
+    }
+
+    public MouseState getState(){
+        return state;
+    }
     public void RemoveBattery() {
-        this.RemoveBattery();
+        state.RemoveBattery();
     }
 
     public void InsertBattery() {
-        this.InsertBattery();
+        state.InsertBattery();
     }
 
     public void BatteryFull() {
-        this.BatteryFull();
+        state.BatteryFull();
     }
 
     public void BatteryEmpty() {
-        this.BatteryEmpty();
+        state.BatteryEmpty();
     }
 }
