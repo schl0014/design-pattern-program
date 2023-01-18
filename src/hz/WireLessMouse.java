2171893cd95c7e2f61f66fd3d898105e3c36023c
niek726 <schl0014@hz.nl>
2023-01-18 09:45:29 +0100

    Update WireLessMouse.java

--- src/hz/WireLessMouse.java
+++ src/hz/WireLessMouse.java
@@ -4,41 +4,48 @@ public class WireLessMouse implements MouseBuilder {
 
     @Override
     public void ConnectToPC() {
-        _mouse += " zoek naar het bluetooth symbooltje zet dit aan. Zoek op je computer naar de bluetotoh functie en connect met je muis" ;
-        this.addNewLine();
+        _mouse+= "Connect to pc";
+        this.BreakLineForTitleLine();
+        _mouse += "Zoek naar het bluetooth symbooltje zet dit aan. Zoek op je computer naar de bluetotoh functie en connect met je muis";
+        this.BreakLineForTitleLine();
     }
 
     public void ClickRate() {
-        _mouse += "clikc een keer op de knop bij de scrollwiel.Dit verhoogt de clickrate met 1 " ;
-        this.addNewLine();
+        _mouse+= "CLick rate";
+        this.BreakLineForTitleLine();
+        _mouse += "click een keer op de knop bij de scrollwiel. Dit verhoogt de clickrate met 1 ";
+        this.BreakLineForTitleLine();
     }
 
     public void Troubleshooting() {
-
-        _mouse += "schakel je muis uit." ;
+        _mouse+= "troubleshooting";
+        this.BreakLineForTitleLine();
+        _mouse += "schakel je muis uit.";
         this.addNewLine();
-        _mouse +="Verwijder de bleutooth verbinding met de muis.";
+        _mouse += "Verwijder de bleutooth verbinding met de muis.";
         this.addNewLine();
-        _mouse +="probeer dan te muis weer te connecten als dit niet werkt update de bluetooth drivers ";
-        this.addNewLine();
+        _mouse += "probeer dan te muis weer te connecten als dit niet werkt update de bluetooth drivers ";
+        this.BreakLineForTitleLine();
     }
 
     public void RgbLight() {
+        _mouse+= "how to turn on RGB";
+        this.BreakLineForTitleLine();
         _mouse += "draai je muis om en zoek naat het licht symbooltje . Druk deze aan";
-        this.addNewLine();
+        this.BreakLineForTitleLine();
     }
 
     public void HowToStore() {
+        _mouse+= "How to shore your mouse";
+        this.BreakLineForTitleLine();
         _mouse += "het beste is om de muis in een apart vakje in je tas te stop zodat het niet beschadigd raakt in je tas";
-        this.addNewLine();
+        this.BreakLineForTitleLine();
     }
-
 
 
     public void reset() {
         _mouse = "";
     }
-
 
 
     public String getMouse() {
@@ -47,5 +54,8 @@ public class WireLessMouse implements MouseBuilder {
 
     private void addNewLine() {
         _mouse += "\n\r";
+    }
+    private void BreakLineForTitleLine() {
+        _mouse += "\n\n\r";
     }
 }
