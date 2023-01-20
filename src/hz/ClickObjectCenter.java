28e302cd2a94300ef39d70fbb01bbb185998f5a7
Jasonjason10 <ofil0001@hz.nl>
2023-01-20 13:38:18 +0100

    added more facades

--- src/hz/ClickObjectCenter.java
+++
@@ -1,5 +0,0 @@
-public class ClickObjectCenter {
-    public void click() {
-        System.out.println("you have clicked on the object in the center");
-    }
-}
---
+++ src/hz/LightsMouse.java
@@ -0,0 +1,22 @@
+public class LightsMouse {
+
+    public void lightOn(){
+        System.out.println("Lights are on");
+    }
+
+    public void lightOff(){
+        System.out.println("Lights are off");
+    }
+
+    public void lightBrightnessUp(int brightness){
+            System.out.println("Brightness increased by"+ brightness);
+    }
+    public void lightBrightnesDown(int brightness){
+        System.out.println("Brightness increased down"+ brightness);
+    }
+
+    public void LightColor(){
+            System.out.println("your color has now changed");
+    }
+
+}
